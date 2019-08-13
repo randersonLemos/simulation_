e00cd398bbd6b183456684c393e802571a7018cd
Randerson <randerson.lemos@gmail.com>
2019-08-13 18:40:25 -0300

    Fisrt integration between run and well modules.

--- main.py
+++ main.py
@@ -1,4 +1,55 @@
-import simulation
+import pathlib
+from settings import *
+from run.scripts import imex
+from well.scripts import wells, utils
 
-print('hi')
-input('Press enter...')
+
+def run_imex(idx, see_log=False, verbose=False):
+    imexx = imex.IMEX(
+              machine = MACHINE
+            , exe = IMEX_EXE
+            , dat_file = IMEX_FOLDER_DAT / 'sim_{:03d}'.format(idx) / 'main.dat'
+            , output_folder = IMEX_FOLDER_OUT / 'sim_{:03d}'.format(idx)
+            , user = USER
+            , cluster_name = CLUSTER_NAME
+            , queue_kind = QUEUE_KIND
+            , see_log = see_log
+            , verbose = verbose
+            )
+    imexx.run()
+    return imexx
+
+
+def generate_producers(idx):
+    path = IMEX_FOLDER_OUT / 'sim_{:03d}'.format(idx)
+    utils.create_dat_at('./well/_dat', str(path))
+
+    Producer    = wells.Producer
+
+    from well.scripts import producers_detail as pd
+
+    keys        = PRODS_KEYS
+    prods_names = PRODS_NAMES
+    prods_times = PRODS_OPEN_TIMES
+    icvs_qtyss  = PRODS_ICVS_QTYS
+
+    ws = []
+    for key in keys:
+        w = Producer(name=prods_names[key], group='PRODUCTION')
+        w.add_operate(maxmin='*MAX', con_type='*STL', value='6000.0', action='*CONT *REPEAT')
+        w.add_operate(maxmin='*MIN', con_type='*BHP', value= '100.0', action='*CONT *REPEAT')
+        w.add_monitor(monitor_type= '*WCUT', value='0.95', action='*SHUTIN *REPEAT')
+        w.add_geometry(dir='*K', rw='0.108', geofac='0.370', wfrac='1.000', skin='0.000')
+        w.add_perf(index_keyword='*GEOA', completion=getattr(pd, prods_names[key]))
+        w.add_availability(avail='1.0')
+        w.add_opening(timsim=prods_times[key])
+        w.write_inc(path / 'wells')
+        ws.append(w)
+    return wells
+
+
+if __name__ == '__main__':
+    for i in range(10):
+        generate_producers(i)
+        run_imex(i, verbose=True)
+
---
+++ settings.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 10 09:04:54 2019
+@author: randerson
+"""
+
+
+from pathlib import Path, PurePosixPath
+
+
+#MACHINE = 'remote'
+MACHINE = 'local'
+if MACHINE == 'local':
+    ROOT = Path('/home/pamonha/simulation')
+    IMEX_EXE = Path('C:/"Program Files (x86)"/CMG/IMEX/2017.10/Win_x64/EXE/mx201710.exe')
+    IMEX_FOLDER_DAT = ROOT / 'results'
+    IMEX_FOLDER_OUT = ROOT / 'results'
+    USER = ''
+    QUEUE_KIND = ''
+    CLUSTER_NAME = ''
+elif MACHINE == 'remote':
+    ROOT = PurePosixPath('/home/randerson/simulation')
+    IMEX_EXE = PurePosixPath('/mnt/simuladores/CMG/imex/2017.10/linux_x64/exe/mx201710.exe')
+    IMEX_FOLDER_DAT = ROOT / 'results'
+    IMEX_FOLDER_OUT = ROOT / 'results'
+    USER = 'randerson'
+    CLUSTER_NAME = 'hpc02'
+    QUEUE_KIND = 'longas'
+else:
+    raise ValueError("MACHINE must be 'local' or 'remote'")
+
+
+#REPORT_EXE = 'C:\\Program Files (x86)\\CMG\\BR\\2017.10\\Win_x64\\EXE\\report.exe'
+#REPORT_IRF = 'U:/simulation/results/main.irf'
+#REPORT_FOLDER_OUT =  'U:/simulation/results'
+
+
+PRODS_KEYS       = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+PRODS_NAMES      = {1:'PRK014', 2:'PRK028', 3:'PRK045', 4:'PRK052', 5:'PRK060', 6:'PRK061', 7:'PRK083', 8:'PRK084', 9:'PRK085', 10:'Pwildc'}
+PRODS_OPEN_TIMES = {1:'1704.0', 2:'1612.0', 3:'1369.0', 4:'1765.0', 5:'1492.0', 6:'1673.0', 7:'1431.0', 8:'1308.0', 9:'1247.0', 10:'1553.0'}
+PRODS_ICVS_QTYS  = {1:3       , 2:2       , 3:3       , 4:2       , 5:2       , 6:3       , 7: 3      , 8:2       , 9:3       , 10:0       }
