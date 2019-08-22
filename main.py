d7e6caa05e7d6f3dfc7f4ed4b4f7859429ff6a04
Randerson Lemos <randerson.lemos@gmail.com>
2019-08-21 21:03:34 -0300

    Small adjustments.

--- main.py
+++ main.py
@@ -1,129 +1,101 @@
-import pathlib
+import log
 from settings import *
 from run.scripts import imex, report
-from well.scripts import wells, utils
 from itertools import cycle
-import time
-import log
+ 
+def generate_producers(well_completion, well_opening, well_on_time, icv_layerclump, icv_start, icv_control, output_folder):
+    from well2 import frames
+    from well2 import utils
+    import pathlib
 
+    for key in well_completion:
+        p = frames._Frame_Prod_Dual(key, 'PRODUCTION')
+        p.get_operate('*MAX', '*STL', 3000.0, '*CONT *REPEAT')
+        p.get_operate('*MIN', '*BHP',  295.0, '*CONT *REPEAT')
+        p.get_monitor('*WCUT', 0.95, '*SHUTIN')
+        p.get_geometry('*K',0.108,0.370,1.0,0.0)
+        p.get_perf('*GEOA')
 
-def run_imex(idx, see_log=False, verbose=False):
+        for com in utils.txt_to_lst(well_completion[key]):
+            p.get_completion(com)
+
+        p.get_on_time(well_on_time[key])
+        p.get_open(well_opening[key])
+
+        for lay in icv_layerclump[key]:
+            p.get_layerclump(lay)
+
+        #p.get_icv_start(icv_start[key])
+        #p.get_icv_control(icv_control[key])
+
+        p.build()
+        p.write(pathlib.Path(output_folder) / '{}.inc'.format(key))
+
+def generate_injectors_wag(well_completion, well_opening, well_on_time, well_wag, output_folder):
+    from well2 import frames
+    from well2 import utils
+    import pathlib
+    for key in well_completion:
+        i = frames._Frame_Inje_Dual_Wag(key, 'INJECTION')
+        i.get_operate('G','*MAX','*STG',3000000.0,'*CONT REPEAT')
+        i.get_operate('G','*MAX','*BHP',    540.0,'*CONT REPEAT')
+        i.get_operate('W','*MAX','*STW',   5000.0,'*CONT REPEAT')
+        i.get_operate('W','*MAX','*BHP',    470.0,'*CONT REPEAT')
+        i.get_geometry('*K',0.108,0.370,1.0,0.0)
+        i.get_perf('*GEOA')
+
+        for com in utils.txt_to_lst(well_completion[key]):
+            i.get_completion(com)
+
+        i.get_on_time(well_on_time[key])
+        i.get_open(*well_opening[key])
+
+        i.get_wag(*well_wag[key])
+
+        i.build()
+        i.write(pathlib.Path(output_folder) / '{}.inc'.format(key))
+           
+if __name__ == '__main__':   
+    import settings as sett            
+    idx = 1
+    import well2.infos1 as infos1
+    generate_producers(infos1.well_completion
+        , infos1.well_opening
+        , infos1.well_on_time
+        , infos1.icv_layerclump
+        , infos1.icv_start
+        , infos1.icv_control
+        , sett.ROOT / sett.IMEX_FOLDER_DAT / 'sim_{:03d}'.format(idx) / 'wells'
+        )   
+    
+    import well2.infos2 as infos2
+    generate_injectors_wag(
+          infos2.well_completion
+        , infos2.well_opening
+        , infos2.well_on_time
+        , infos2.well_wag
+        , sett.ROOT / sett.IMEX_FOLDER_DAT / 'sim_{:03d}'.format(idx) / 'wells'
+        )
+     
+    root = sett.ROOT_LOCAL
+    if sett.MACHINE == 'remote': root = sett.ROOT_REMOTE
     imexx = imex.IMEX(            
-              machine = MACHINE
-            , idx = idx
-            , exe = IMEX_EXE
-            , dat_file = ROOT / IMEX_FOLDER_DAT / 'sim_{:03d}'.format(idx) / 'main.dat'
-            , output_folder = ROOT / IMEX_FOLDER_OUT / 'sim_{:03d}'.format(idx)
-            , user = USER
-            , cluster_name = CLUSTER_NAME
-            , queue_kind = QUEUE_KIND
-            , see_log = see_log
-            , verbose = verbose
-            )
+          sett.MACHINE
+        , sett.IMEX_EXE
+        , root / sett.IMEX_FOLDER_DAT / 'sim_{:03d}'.format(idx) / 'main.dat'
+        , root / sett.IMEX_FOLDER_OUT / 'sim_{:03d}'.format(idx)
+        , sett.USER
+        , sett.CLUSTER_NAME
+        , sett.QUEUE_KIND
+        , see_log = True
+        , verbose = True
+        )
     imexx.run()
-    return imexx
-
-
-def run_report(idx, verbose=False):
-    root = Path(ROOT)
-    if MACHINE == 'remote':
-        root = Path(ROOT_LOCAL)
-        
+    
     repor = report.REPORT(
-              exe = REPORT_EXE
-            , irf_file = root / REPORT_FOLDER_IRF / 'sim_{:03d}'.format(idx) / 'main.irf'
-            , output_folder = root / REPORT_FOLDER_OUT / 'sim_{:03d}'.format(idx)
-            , verbose = verbose
-            )
+          exe = sett.REPORT_EXE
+        , irf_file = sett.ROOT_LOCAL / sett.REPORT_FOLDER_IRF / 'sim_{:03d}'.format(idx) / 'main.irf'
+        , output_folder = sett.ROOT_LOCAL / sett.REPORT_FOLDER_OUT / 'sim_{:03d}'.format(idx)
+        , verbose = True
+        )
     repor.run()
-
-
-def generate_producers(idx, verbose=False):
-    path = Path(ROOT / IMEX_FOLDER_OUT / 'sim_{:03d}'.format(idx))
-    if MACHINE == 'remote':
-        path = Path(ROOT_LOCAL / IMEX_FOLDER_OUT / 'sim_{:03d}'.format(idx))
-    if verbose: print('generate_producers path:\n\t{}'.format(path))
-    path.mkdir(parents=True, exist_ok=True)
-    utils.create_dat_at('./well/_dat', str(path))
-
-    from well.scripts import producers_detail as pd
-
-    Producer    = wells.Producer
-
-    keys        = PRODS_KEYS
-    prods_names = PRODS_NAMES
-    prods_times = PRODS_OPEN_TIMES
-
-    ws = []
-    for key in keys:
-        w = Producer(name=prods_names[key], group='PRODUCTION')
-        w.add_operate(maxmin='*MAX', con_type='*STL', value='3000.0', action='*CONT *REPEAT')
-        w.add_operate(maxmin='*MIN', con_type='*BHP', value= '295.0', action='*CONT *REPEAT')
-        w.add_monitor(monitor_type= '*WCUT', value='0.95', action='*SHUTIN *REPEAT')
-        w.add_geometry(dir='*K', rw='0.108', geofac='0.370', wfrac='1.000', skin='0.000')
-        w.add_perf(index_keyword='*GEOA', completion=getattr(pd, prods_names[key]))
-        w.add_availability(avail='1.0')
-        w.add_opening(timsim=prods_times[key])
-        w.write_inc(path / 'wells')
-        ws.append(w)
-    return ws
-
-
-def generate_injectors(idx, verbose=False):
-    path = Path(ROOT / IMEX_FOLDER_OUT / 'sim_{:03d}'.format(idx))
-    if MACHINE == 'remote':
-        path = Path(ROOT_LOCAL / IMEX_FOLDER_OUT / 'sim_{:03d}'.format(idx))
-    if verbose: print('generate_injectors path:\n\t{}'.format(path))
-    path.mkdir(parents=True, exist_ok=True)
-    utils.create_dat_at('./well/_dat', str(path))
-
-    from well.scripts import injectors_detail as idd
-
-    Injector_WAG = wells.Injector_WAG
-
-    keys = INJES_KEYS
-    injs_names = INJES_NAMES
-    injs_times = INJES_OPEN_TIMES
-    injs_wagss = INJES_WAGSS
-
-    ws = []
-    for key in keys:
-        w = Injector_WAG(name=injs_names[key], group='INJECTION')
-        w.add_operate(which_mode='GAS', maxmin='*MAX', con_type='*STG', value='3000000.0', action='*CONT *REPEAT')
-        w.add_operate(which_mode='GAS', maxmin='*MAX', con_type='*BHP', value='540.0'    , action='*CONT *REPEAT')
-        w.add_operate(which_mode='WATER', maxmin='*MAX', con_type='*STW', value='5000.0', action='*CONT *REPEAT')
-        w.add_operate(which_mode='WATER', maxmin='*MAX', con_type='*BHP', value='470.0' , action='*CONT *REPEAT')
-        w.add_geometry(dir='*K', rw='0.108', geofac='0.370', wfrac='1.000', skin='0.000')
-        w.add_perf(index_keyword='*GEOA', completion=getattr(idd, injs_names[key]))
-        w.add_availability(avail='1.0')
-        w.add_opening(which_mode=injs_times[key][0], timsim=injs_times[key][1])
-        w.add_wag_strategy(start_mode=injs_wagss[key], timsim='1918.0', delta_timsim='180.0'
-                , apply_times='100', increment='366')
-        w.write_inc(path / 'wells')
-        ws.append(w)
-    return ws
-
-
-
-if __name__ == '__main__':
-    for idx in range(N_SIMS):
-        generate_producers(idx, verbose=True)
-        generate_injectors(idx, verbose=True)        
-    
-    sims = []
-    for idx in range(N_SIMS):
-        sims.append(run_imex(idx, see_log=False, verbose=True))
-    
-    while sims:
-        for idx, sim in enumerate(sims):
-            if sim.is_alive():
-                print("******SIMULATION 'sim_{:03d}'******".format(idx))
-                log.Log.see(idx)
-                print("********************************")
-                time.sleep(30)
-            else:
-                print("Simulation 'sim_{:03d}' fineshed. Generating report.".format(idx))                
-                run_report(sims[idx].idx, verbose=True)
-                del sims[idx]
-                break
-            
--- settings.py
+++ settings.py
@@ -3,50 +3,32 @@
 Created on Sat Aug 10 09:04:54 2019
 @author: randerson
 """
-
-
-from pathlib import Path, PurePosixPath
-
+from pathlib import Path
 
 #N_SIMS = 50
 
-
-#MACHINE = 'remote'
+MACHINE = 'remote'
 #MACHINE = 'local'
-if MACHINE == 'local':
-    ROOT = Path('U:/simulation')
+if MACHINE == 'local':    
+    ROOT_LOCAL = Path('U:/simulation')
     IMEX_EXE = Path('C:/"Program Files (x86)"/CMG/IMEX/2017.10/Win_x64/EXE/mx201710.exe')
-    IMEX_FOLDER_DAT = 'results'
-    IMEX_FOLDER_OUT = 'results'
+    IMEX_FOLDER_DAT = 'sims'
+    IMEX_FOLDER_OUT = 'sims'
     USER = ''
     QUEUE_KIND = ''
     CLUSTER_NAME = ''
-elif MACHINE == 'remote':
-    ROOT = PurePosixPath('/home/randerson/simulation')
+if MACHINE == 'remote':
     ROOT_LOCAL = Path('U:/simulation')
-    IMEX_EXE = PurePosixPath('/mnt/simuladores/CMG/imex/2017.10/linux_x64/exe/mx201710.exe')
-    IMEX_FOLDER_DAT =  'results'
-    IMEX_FOLDER_OUT =  'results'
+    ROOT_REMOTE = Path('/home/randerson/simulation')
+
+    IMEX_EXE = Path('/mnt/simuladores/CMG/imex/2017.10/linux_x64/exe/mx201710.exe')
+    IMEX_FOLDER_DAT =  'sims'
+    IMEX_FOLDER_OUT =  'sims'
     USER = 'randerson'
     CLUSTER_NAME = 'hpc02'
     QUEUE_KIND = 'longas'
     PUTTY_EXE = Path('C:/\"Program Files (x86)"/PuTTY/plink.exe')
-else:
-    raise ValueError("MACHINE must be 'local' or 'remote'")
 
-
-REPORT_EXE = 'C:\\Program Files (x86)\\CMG\\BR\\2017.10\\Win_x64\\EXE\\report.exe'
-REPORT_FOLDER_IRF = 'results'
-REPORT_FOLDER_OUT = 'results'
-
-
-PRODS_KEYS       = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-PRODS_NAMES      = {1:'PRK014', 2:'PRK028', 3:'PRK045', 4:'PRK052', 5:'PRK060', 6:'PRK061', 7:'PRK083', 8:'PRK084', 9:'PRK085', 10:'Pwildc'}
-PRODS_OPEN_TIMES = {1:'1704.0', 2:'1612.0', 3:'1369.0', 4:'1765.0', 5:'1492.0', 6:'1673.0', 7:'1431.0', 8:'1308.0', 9:'1247.0', 10:'1553.0'}
-PRODS_ICVS_QTYS  = {1:3       , 2:2       , 3:3       , 4:2       , 5:2       , 6:3       , 7: 3      , 8:2       , 9:3       , 10:0       }
-
-
-INJES_KEYS = [1, 2, 3, 4, 5, 6, 7, 8]
-INJES_NAMES = {1:'IRK004', 2:'IRK028', 3:'IRK029', 4:'IRK036', 5:'IRK049', 6:'IRK050', 7:'IRK056', 8:'IRK063'}
-INJES_OPEN_TIMES = {1:['WATER','1734.0'], 2:['WATER','1643.0'], 3:['WATER','1400.0'], 4:['GAS','1584.0'], 5:['GAS','1522.0'], 6:['WATER','1461.0'], 7:['GAS','1339.0'], 8:['GAS','1278.0']}
-INJES_WAGSS = {1:'GAS', 2:'GAS', 3:'GAS', 4:'WATER', 5:'WATER', 6:'GAS', 7:'WATER', 8:'WATER'}
+REPORT_EXE = Path('C:/\"Program Files (x86)"/CMG/BR/2017.10/Win_x64/EXE/report.exe')
+REPORT_FOLDER_IRF = 'sims'
+REPORT_FOLDER_OUT = 'sims'
